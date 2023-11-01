d9e98b5b85e53f8b9215340bf9b3473715116188
Will Churchill <willchurchill89@gmail.com>
2023-11-01 15:56:15 +0000

    changing to deploy from static files

--- docs/_includes/analytics.js
+++ docs/_includes/analytics.js
@@ -3,25 +3,3 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-KTETBDVBN2');
-
-/*
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-59QDK8R');
-
-  */
-/*
-var script = document.createElement('script');
-script.type = 'text/javascript';
-
-script.src = 'https://www.googletagmanager.com/gtag/js?id=G-KTETBDVBN2';
-document.body.appendChild(script);
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-KTETBDVBN2');
-*/
